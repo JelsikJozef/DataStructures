636736e751715f2e7ba32f309d2436c10aba9f53
JelsikJozef <jozefjelsik@gmail.com>
2025-03-21 19:39:42 +0100

    all filters applied

--- JelsikAUS/src/main.cpp
+++ JelsikAUS/src/main.cpp
@@ -15,17 +15,33 @@ int main() {
     CSVReader reader;
     std::vector<Stop> stops = reader.readCSV("./subory/GRT_Stops_Relevant.csv");
 
+	//Declaration of FilterAlgorithm
     FilterAlgorithm algo;
 
+	//1. Filter - Municipality
     auto municipalityFilter = isInMunicipality("Kitchener");
     auto filteredStops = algo.filter(stops.begin(), stops.end(), municipalityFilter);
 
+	std::cout << "Stops in Kitchener: \n";
     for (const auto& s : filteredStops) {
 		std::cout << "StopID: " << s.stop_ID() << ", Municipality: " << s.municipality()<< "\n";
     }
+	//2. Filter - Street
+	auto streetFilter = isOnStreet("King");
+	filteredStops = algo.filter(stops.begin(), stops.end(), streetFilter);
 
+	std::cout << "Stops on King Street: \n";
+	for (const auto& s : filteredStops) {
+		std::cout << "StopID: " << s.stop_ID() << ", Street: " << s.street() << "\n";
+	}
+	//3. Filter - Region
+	auto regionFilter = isInRegion(43.4, 43.5, -80.6, -80.5);
+	auto regionStops = algo.filter(stops.begin(), stops.end(), regionFilter);
 
-
+	std::cout << "Stops in Region: \n";
+	for (const auto& s : regionStops) {
+		std::cout << "StopID: " << s.stop_ID() << ", Latitude: " << s.latitude() << ", Longitude: " << s.longitude() << "\n";
+	}
 
 
     return 0;
